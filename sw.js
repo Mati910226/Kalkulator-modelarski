bb50dd9295bddd9d5cbd412853819099ee7482c1
Mati910226 <mateusz.matsp@gmail.com>
2026-06-28 21:58:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,10 @@
-const CACHE_NAME = "kalkulator-modelarski-v2";
+const CACHE_NAME = "kalkulator-modelarski-private-v1";
 const FILES = ["./", "./index.html", "./manifest.json"];
-self.addEventListener("install", e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES))));
-self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
